dfbe217876a865eda23dce87e159ce315a6f07ab
Chris Matabaro <christianmatabaro@gmail.com>
2023-03-09 17:58:23 +0300

    Deploy

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -50,7 +50,7 @@ const start = async () => {
     console.error(err);
   }
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!');
+    console.log('Listening on port 3000!!!');
   });
 };
 
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -49,7 +49,7 @@ const start = async () => {
     console.error(err);
   }
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!');
+    console.log('Listening on port 3000!!!');
   });
 };
 
